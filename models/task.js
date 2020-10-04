1f95ed5dc8b8d392de9272b6b525702f9f98afe4
unknown <lauromilagres@gmail.com>
2020-10-04 10:48:56 -0300

    tela 2

---
+++ models/task.js
@@ -0,0 +1,9 @@
+class Task {
+    constructor(){
+        this.status = false;
+        this.nome = '';
+        this.descricao = '';
+        this.startDate = '';
+        this.endDate = '';
+    }
+}
--- tolearning.js
+++ tolearning.js
@@ -1,11 +1,21 @@
 
+onload = () => {
 
-
-function newTask(){
-    document.getElementById("tela1").style.visibility = 'hidden';
-    document.getElementById("tela2").style.visibility = 'visible';
+    function newTask(){
+        document.getElementById("tela1").style.display = 'none';
+        document.getElementById("tela2").style.visibility = 'visible';
+    }
+    
+    function saveTask(){
+        this.backToList();
+    }
+    
+    function editTask(id){
+    
+    }
 }
 
 function backToList(){
-    document.getElementById("tela1").style.visibility = 'visible';
+    document.getElementById("tela1").style.display = 'block';
+    document.getElementById("tela2").style.visibility = 'hidden';
 }
