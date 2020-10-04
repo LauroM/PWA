d7fd344613b7506364a6bfda591ec3a484af945f
unknown <lauromilagres@gmail.com>
2020-10-04 08:39:52 -0300

    gambiarra

---
+++ tolearning.js
@@ -0,0 +1,11 @@
+
+
+
+function newTask(){
+    document.getElementById("tela1").style.visibility = 'hidden';
+    document.getElementById("tela2").style.visibility = 'visible';
+}
+
+function backToList(){
+    document.getElementById("tela1").style.visibility = 'visible';
+}
